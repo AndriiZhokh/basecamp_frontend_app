ede026741513a3f8d93898aa103818fb08edd21e
Andriy7172 <andriyko.zoh5@gmail.com>
2019-02-15 18:37:31 +0200

    Implemented fetch POST request to server for adding new show to database

--- src/components/AddShowForm/AddShowForm.js
+++ src/components/AddShowForm/AddShowForm.js
@@ -8,89 +8,149 @@ class AddShowForm extends Component {
     this.state = {
       title: '',
       subtitle: '',
-      selectedFile: null,
-      loaded: 0
+      date_of_start: '',
+      long: '',
+      short: '',
+      url: '',
+      priority: '',
+      rating: '',
     };
+    this.fileInput = React.createRef();
   }
 
-  handleSelectedFile = (event) => {
-    this.setState({
-      selectedFile: event.target.files[0]
+  sendData = (url, data, file) => {
+    console.log(file.current.files[0]);
+    const formData = new FormData();
+
+    for(const name in data) {
+      formData.append(name, data[name])
+    }
+
+    formData.append('image', file.current.files[0], file.current.files[0].name);
+
+    for(const name of formData) {
+      console.log(name);
+    }
+
+    fetch(url, {
+      method: 'POST',
+      body: formData
     });
-  }
-
-  handleUpload = () => {
-    const data = new FormData();
-    data.append('file', this.state.selectedFile, this.state.selectedFile.name);
-
-    axios.post('/upload', data);
   }
 
   handleChange = (event) => {
     const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
-
-    this.setState({ [name]: value })
+    this.setState({[name]: event.target.value});
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state);
-
-    const options = {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      mode: 'cors',
-      body: JSON.stringify(this.state)
-    }
-
-    const request = new Request('/show', options)
-
-    fetch(request);
+    /* console.log(this.fileInput.current.files[0].name);
+    console.log(this.state.title);
+    console.log(this.state.subtitle);
+    console.log(this.state.date_of_start);
+    console.log(this.state.long);
+    console.log(this.state.short);
+    console.log(this.state.priority);
+    console.log(this.state.rating);
+    console.log(this.state.url); */
+    this.sendData('/addshow', this.state, this.fileInput);
   }
-
+  
   render() {
     return(
-      <form className = "add-show-form">
-        {/* <label htmlFor = "title">Enter Title{this.state.title}</label>
-        <input id = "title" 
-               name = "title" 
-               value = {this.state.title} 
-               onChange = {this.handleChange} 
-               type = "text" required/>
+      <form className = "add-show-form" onSubmit = {this.handleSubmit}>
+        <label >Enter Title
+          <input id = "title" 
+                 name = "title" 
+                 value = {this.state.title} 
+                 onChange = {this.handleChange} 
+                 type = "text" required/>
+                 {this.state.title}
+        </label>
+        
+        <label>Enter SubTitle
+          <input id = "subtitle" 
+                 name = "subtitle"
+                 value = {this.state.subtitle}
+                 onChange = {this.handleChange}
+                 type = "text" required/>
+                 {this.state.subtitle}
+        </label>
 
-        <label htmlFor = "subtitle">Enter SubTitle</label>
-        <input id = "subtitle" 
-               name = "subtitle"
-               value = {this.state.subtitle}
-               onChange = {this.handleChange}
-               type = "text" required/> */}
+        <label>Chose date of start
+          <input id = "date-of-start" 
+                 name = "date_of_start" 
+                 type = "date" 
+                 value = {this.state.date_of_start}
+                 onChange = {this.handleChange}
+                 required/>
+                 {this.state.date_of_start}
+        </label>
 
-        {/* <label htmlFor = "date-of-start">Chose date of start</label>
-        <input id = "date-of-start" name = "date-of-start" type = "date" required/> */}
+        <label>Long Description
+          <textarea id = "long" 
+                    name = "long" 
+                    value = {this.state.long}
+                    onChange = {this.handleChange}
+                    required/>
+                    {this.state.long}
+        </label>
+        
+        <label>Short Description
+          <textarea id = "short" 
+                    name = "short" 
+                    value = {this.state.short}
+                    onChange = {this.handleChange}
+                    required />
+                    {this.state.short}
+        </label>
+
+        <label>Video URL
+          <input id ="url" 
+                 name = "url" 
+                 type = "url" 
+                 value = {this.setState.url}
+                 onChange = {this.handleChange}
+                 required/>
+                 {this.state.url}
+        </label>
+
+        <label>Priority
+          <select name = "priority" value = {this.state.prioryty} onChange = {this.handleChange}>
+            <option value = "1">1</option>
+            <option value = "2">2</option>
+            <option value = "3">3</option>
+            <option value = "4">5</option>
+            <option value = "5">5</option>
+          </select>
+          {this.state.priority} 
+        </label>
+
+        <label>User rating
+          <select name = "rating" value = {this.state.rating} onChange = {this.handleChange}>
+            <option value = "1">1</option>
+            <option value = "2">2</option>
+            <option value = "3">3</option>
+            <option value = "4">4</option>
+            <option value = "5">5</option>
+            <option value = "6">6</option>
+            <option value = "7">7</option>
+            <option value = "8">8</option>
+            <option value = "9">9</option>
+            <option value = "10">10</option>
+          </select>
+          {this.state.rating}
+        </label>
+
         <div>
           <label htmlFor = "image">Chose Poster</label>
           <input id = "image" 
                  name = "image" 
                  type = "file"
-                 onChange = {this.handleSelectedFile}/>
-          <div> {Math.round(this.state.loaded,2) } %</div>
-          <button onClick = {this.handleUpload}>Upload</button>
+                 ref = {this.fileInput}/>
         </div>
-
-
-        {/* <textarea name = "long" placeholder = "Long Description" required></textarea>
-        <textarea name = "short" placeholder = "Short Description" required></textarea>
-        <select name = "priority">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-        </select>
-        <input name = "url" placeholder = "video url" type = "url" required/> */}
         <button type = "submit">Send data</button>
       </form>
     );
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,15 +6,15 @@ import Admin from '../Admin/Admin';
 class App extends Component {
   state = { users: [] }
 
-  componentDidMount(){
+/*   componentDidMount(){
     fetch('/users')
       .then(res => res.json())
       .then(users => this.setState({ users }))
-  }
+  } */
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
+        <h1>Serials</h1>
         <ul>
           {this.state.users.map(user => 
             <li key = {user.id}>{user.title} | {user.date_of} | {user.priority}</li>)}
