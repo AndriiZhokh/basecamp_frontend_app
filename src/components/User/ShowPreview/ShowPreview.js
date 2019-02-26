6a66ff8b364380451715cf6387341663221113a0
Andriy7172 <andriyko.zoh5@gmail.com>
2019-02-26 19:12:16 +0200

    Created ShowPreview component

---
+++ src/components/User/ShowPreview/ShowPreview.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default class ShowPreview extends Component {
+  render() {
+    const { show } = this.props;
+    return (
+      <div key = {show.id}>
+        <div className="col s12 m6">
+          <div className="card">
+            <div className="card-image">
+              <img src={`http://localhost:3002/images/${show.poster_image}`} alt = 'poster'/>
+              <span className="card-title">{show.subtitle}</span>
+            </div>
+            <div className="card-content">
+              <Link to = {`/show_details/${show.id}`}>{show.title}</Link>
+              <p>{show.short_description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
