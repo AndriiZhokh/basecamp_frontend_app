fdb17f721654752275b79e3d0c074858d1f9e104
Andriy7172 <andriyko.zoh5@gmail.com>
2019-03-12 18:04:32 +0200

    Changed url for fetch in PopularShow component

--- src/components/User/PopularShows/PopularShows.js
+++ src/components/User/PopularShows/PopularShows.js
@@ -10,7 +10,7 @@ export default class PopularShows extends Component {
   }
 
   componentDidMount() {
-    fetch('/shows')
+    fetch('/show')
       .then(res => res.json())
       .then(data => this.setState({shows: data}))
       .catch(err => console.log(err));
